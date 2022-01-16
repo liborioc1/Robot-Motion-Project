2c2f4ab31db27448cf5eaf0fbdab24629175f0c1
liborioc1 <liboriociccarello2@gmail.com>
2022-01-16 16:19:24 -0500

    #8 Print floor function implemented

--- src/main/java/com/gradle/robotmotion/Floor.java
+++ src/main/java/com/gradle/robotmotion/Floor.java
@@ -23,4 +23,25 @@ public class Floor {
         floorSize = size;
         floor = new int[floorSize][floorSize];
     }
+
+    public void printFloor(){
+        System.out.println("Floor is of size: " + floorSize + " x " + floorSize);
+
+        for(int i = floorSize-1 ; i >= 0 ; i--){
+            System.out.print(i + " ");
+            for(int j = 0 ; j < floorSize ; j++){
+                if(floor[i][j] == 1) {
+                    System.out.print("*" + " ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.print("  ");
+        for(int i = 0; i < floorSize; i++){
+            System.out.print(i + " ");
+        }
+    }
 }
