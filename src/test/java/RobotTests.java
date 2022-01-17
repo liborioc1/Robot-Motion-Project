f2f28aba837509e60175b366ed5799aece2a75e5
NabilahRouf <nabilah_rouf@live.ca>
2022-01-16 22:35:05 -0500

    #3 #4 #9 Wrote unit tests for pen-up, pen-down and print current position.

--- src/test/java/RobotTests.java
+++ src/test/java/RobotTests.java
@@ -14,31 +14,41 @@ public class RobotTests {
     static Floor floor;
     static Robot robot;
 
-    @BeforeAll
-    public static void setUp(){
+    @Test
+    public void testGetOrientation(){
         floor = new Floor();
         robot=new Robot(floor);
         floor.initializeFloor(10);
-    }
 
-    @Test
-    public void testGetOrientation(){
         Assertions.assertEquals(Robot.Orientation.NORTH, robot.getOrientation());
     }
 
     @Test
     public void testGetPenUp(){
-        Assertions.assertEquals(true, robot.getPenUp());
+        floor = new Floor();
+        robot=new Robot(floor);
+        floor.initializeFloor(10);
+
+        Assertions.assertTrue(robot.getPenUp());
     }
+
 
     @Test
     public void testGetCurrentPosition(){
+        floor = new Floor();
+        robot=new Robot(floor);
+        floor.initializeFloor(10);
+
         Position defaultPosition = new Position(0,0);
         Assertions.assertEquals(defaultPosition.toString(), robot.getCurrentPosition().toString());
     }
 
     @Test
     public void testTurnRight(){
+        floor = new Floor();
+        robot=new Robot(floor);
+        floor.initializeFloor(10);
+
         robot.turnRight();
         Assertions.assertEquals(Robot.Orientation.EAST,robot.getOrientation());
         robot.turnRight();
@@ -51,6 +61,10 @@ public class RobotTests {
 
     @Test
     public void testTurnLeft(){
+        floor = new Floor();
+        robot=new Robot(floor);
+        floor.initializeFloor(10);
+
         robot.turnLeft();
         Assertions.assertEquals(Robot.Orientation.WEST,robot.getOrientation());
         robot.turnLeft();
@@ -61,10 +75,42 @@ public class RobotTests {
         Assertions.assertEquals(Robot.Orientation.NORTH,robot.getOrientation());
     }
 
-    @AfterAll
-    public static void teardown(){
-        floor = null;
-        robot=null;
+    @Test
+    public void testPenUp() {
+        floor = new Floor();
+        robot=new Robot(floor);
+        floor.initializeFloor(10);
+
+        robot.penUp();
+        Assertions.assertTrue(robot.getPenUp());
+
     }
+
+    @Test
+    public void testPenDown() {
+        floor = new Floor();
+        robot=new Robot(floor);
+        floor.initializeFloor(10);
+
+        robot.penDown();
+        Assertions.assertFalse(robot.getPenUp());
+
+    }
+
+    @Test
+    public void testPrintCurrentPosition() {
+        floor = new Floor();
+        robot=new Robot(floor);
+        floor.initializeFloor(10);
+
+        robot.penUp();
+        Assertions.assertEquals("Position: {0,0} - Pen: Up - Facing: NORTH","Position: " + robot.getCurrentPosition().toString() + " - Pen: Up - Facing: " + robot.getOrientation().toString());
+
+        robot.penDown();
+        Assertions.assertEquals("Position: {0,0} - Pen: Down - Facing: NORTH","Position: " + robot.getCurrentPosition().toString() + " - Pen: Down - Facing: " + robot.getOrientation().toString());
+
+
+    }
+
 
 }
